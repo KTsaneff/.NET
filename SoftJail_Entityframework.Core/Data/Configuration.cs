9db2d764ee1717c4d5af5b0bbbc2c92aa4cf0349
krassytsaneff@gmail.com <jealousy01>
2022-10-08 14:25:12 +0300

    Tsaneff

--- SoftJail_Entityframework.Core/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=localhost, 1433;Database=SoftJail;User Id=sa;Password=JEAlousy01";
-    }
-}
