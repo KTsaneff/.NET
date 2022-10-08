482f71383a3b2a687cd3cdbd5a9ec8b472adc636
krassytsaneff@gmail.com <jealousy01>
2022-10-08 14:26:47 +0300

    Tsaneff rename

---
+++ SoftJail_EFCore/Data/Models/Cell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public class Cell
+    {
+        public Cell()
+        {
+            this.Prisoners = new HashSet<Prisoner>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public int CellNumber { get; set; }
+
+        public bool HasWindow { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        public virtual ICollection<Prisoner> Prisoners { get; set; }
+    }
+}
---
+++ SoftJail_EFCore/DataProcessor/ImportDto/ImportPrisonerMailDto.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SoftJail.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class ImportPrisonerMailDto
+    {
+        [Required]
+        [JsonProperty(nameof(Description))]
+        public string Description { get; set; }
+
+        [Required]
+        [JsonProperty(nameof(Sender))]
+        public string Sender { get; set; }
+
+        [Required]
+        [RegularExpression(ValidationConstants.MailAddressRegex)]
+        [JsonProperty(nameof(Address))]
+        public string Address { get; set; }
+    }
+}
