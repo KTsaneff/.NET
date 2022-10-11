e0f56dab573dc359d07c71e39d99284f04a4a3e1
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2022-10-11 15:18:56 +0300

    Delete source/SoftUni/SoftJail_EFCore directory

--- source/SoftUni/SoftJail_EFCore/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=.;Database=SoftJail;User Id=.;Password=.";
-    }
-}
