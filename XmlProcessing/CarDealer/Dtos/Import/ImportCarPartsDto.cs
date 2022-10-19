d5227e10b39d914ff9031b8f90ac90b9ce14fa05
krassytsaneff@gmail.com <jealousy01>
2022-10-19 21:57:16 +0300

    Xml Serialize/Deserialize

---
+++ XmlProcessing/CarDealer/Dtos/Import/ImportCarPartsDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.Dtos.Import
+{
+    [XmlType("partId")]
+    public class ImportCarPartsDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+    }
+}
