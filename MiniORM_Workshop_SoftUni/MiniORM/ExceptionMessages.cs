15a1a6b1019fba2e9eb919f834fc67a001641073
krassytsaneff@gmail.com <jealousy01>
2022-10-25 14:49:52 +0300

    SoftUni_OrmFundamentals_CustomMiniORM

---
+++ MiniORM_Workshop_SoftUni/MiniORM/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniORM
+{
+    internal static class ExceptionMessages
+    {
+        internal const string ItemNullException = "Item cannot be null!";
+        internal const string InvalidEntitiesInCtx = "{0} Invalid Entities found in {1}!";
+    }
+}
