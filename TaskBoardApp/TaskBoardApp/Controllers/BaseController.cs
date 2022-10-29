e6130ae590724477cb6112cebf298394db077d00
krassytsaneff@gmail.com <jealousy01>
2022-10-29 09:02:10 +0300

    Login&&Register_Added

---
+++ TaskBoardApp/TaskBoardApp/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskBoardApp.Controllers
+{
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
