0446327d2581e5615fd2ee6abc888689d72daf51
krassytsaneff@gmail.com <jealousy01>
2022-11-06 15:39:47 +0200

    Adding new features

---
+++ WatchListDemo/Watchlist/Controllers/MoviesController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Watchlist.Contracts;
+using Watchlist.Models;
+using Watchlist.Services;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Security.Claims;
+
+namespace Watchlist.Controllers
+{
+    [Authorize]
+    public class MoviesController : Controller
+    {
+        private readonly IMovieService movieService;
+
+        public MoviesController(IMovieService _movieService)
+        {
+            this.movieService = _movieService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await movieService.GetAllAsync();
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new AddMovieViewModel()
+            {
+                Genres = await movieService.GetGenresAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddMovieViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await movieService.AddMovieAsync(model);
+
+                return RedirectToAction(nameof(All));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Something went wrong!");
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCollection(int movieId)
+        {
+            try
+            {
+                var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+                await movieService.AddMovieToCollectionAsync(movieId, userId);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public async Task<IActionResult> Watched()
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var model = await movieService.GetWatchedAsync(userId);
+
+            return View("Mine", model);
+        }
+
+        public async Task<IActionResult> RemoveFromCollection(int movieId)
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            await movieService.RemoveMovieFromCollectionAsync(movieId, userId);
+
+            return RedirectToAction(nameof(Watched));
+        }
+    }
+}
---
+++ WatchListDemo/Watchlist/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace Watchlist.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal actualValue = 0M;
+                bool success = false;
+
+                try
+                {
+                    string decimalValue = valueResult.FirstValue;
+                    decimalValue = decimalValue
+                        .Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    decimalValue = decimalValue
+                        .Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    actualValue = Convert
+                        .ToDecimal(decimalValue, CultureInfo.CurrentCulture);
+                    success = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (success)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(actualValue);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
