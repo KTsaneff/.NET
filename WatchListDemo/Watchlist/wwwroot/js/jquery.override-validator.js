7684f40cc470026aa5117e01057dafccb5a88823
krassytsaneff@gmail.com <jealousy01>
2022-11-07 13:26:12 +0200

    Minor edits

--- WatchListDemo/Watchlist/wwwroot/js/jquery.override-validator.js
+++ WatchListDemo/Watchlist/wwwroot/js/jquery.override-validator.js
@@ -1,5 +1,5 @@
 ﻿jQuery(function ($) {
 	$.validator.addMethod('number', function (value, element) {
-		return this.optional(element) || /^(?:-?\d +)(?:(\.|,)\d+)?$/.test(value);
+		return this.optional(element) || /^(?:-?\d+)(?:(\.|,)\d+)?$/.test(value);
 	})
 });
