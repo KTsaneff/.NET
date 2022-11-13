97c1a4ed7b8e94cf7d28956dfb0f00f3cf7493c9
krassytsaneff@gmail.com <jealousy01>
2022-11-13 17:25:36 +0200

    NewDemoProject_Started

---
+++ Artico/Artico/Controllers/ApplicationUserController.cs
@@ -0,0 +1,110 @@
+﻿using Artico.Core.Data.Models;
+using Artico.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Artico.Controllers
+{
+    public class ApplicationUserController : BaseController
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<ApplicationUser> roleManager;
+
+        public ApplicationUserController(
+            UserManager<ApplicationUser> _userManager, 
+            SignInManager<ApplicationUser> _signInManager, 
+            RoleManager<ApplicationUser> _roleManager)
+        {
+            this.userManager = _userManager;
+            this.signInManager = _signInManager;
+            this.roleManager = _roleManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel();
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                JobId = model.JobId,
+                PositionId = model.PositionId
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "ApplicationUser");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Home", "Index");
+            }
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Home", "Index");
+                }
+            }
+            ModelState.AddModelError("", "Invalid login!");
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
