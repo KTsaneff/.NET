3ad2943f57d08c963733589f5be9dfab512bffc4
krassytsaneff@gmail.com <jealousy01>
2023-03-06 12:48:57 +0200

    Authentication and Authorization

---
+++ Artico/Artico/Services/Contracts/IArticleService.cs
@@ -0,0 +1,22 @@
+﻿using Artico.Core.Data.Models;
+using Artico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artico.Core.Contracts
+{
+    public interface IArticleService
+    {
+        Task<IEnumerable<ArticleViewModel>> GetAllAsync();
+
+        Task<IEnumerable<Job>> GetJobAsync();
+
+        Task<IEnumerable<Position>> GetPositionAsync();
+
+        Task AddArticleAsync(CreateArticleViewModel model);
+
+    }
+}
---
+++ Artico/Artico/Services/Services/ApplicationUserService.cs
@@ -0,0 +1,32 @@
+﻿using Artico.Core.Contracts;
+using Artico.Core.Data;
+using Artico.Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artico.Core.Services
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        private readonly ApplicationDbContext context;
+
+        public ApplicationUserService(ApplicationDbContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<IEnumerable<Job>> GetJobAsync()
+        {
+            return await context.Jobs.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Position>> GetPositionAsync()
+        {
+            return await context.Positions.ToListAsync();
+        }
+    }
+}
